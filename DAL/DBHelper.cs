d052ef2a0d311642a8a1325d12e689d27e5bcaa9
lehuygiang28 <lehuygiang28@gmail.com>
2021-08-30 10:44:05 +0700

    Add DAL

---
+++ DAL/DBHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MySql.Data;
+
+namespace DAL
+{
+    public class DBHelper
+    {
+        public class DBHelper
+        {
+            private static MySqlconnection connection;
+            public static MySqlconnection GetConnection()
+            {
+                if (connection == null)
+                {
+                   connection = new MySqlConnection
+                {
+                    ConnectionString = "server=localhost;user id=vtca;password=vtcacademy;port=3306;database=LoginDB;"
+                };
+                }
+            }
+        }
+    }
+}
--- Persistance/Staff.cs
+++ Persistance/Staff.cs
@@ -4,5 +4,18 @@ namespace Persistance
 {
     public class Staff
     {
+        public int StaffId {set; get;}
+        public string UserName {set; get;}
+        public string Password {set; get;}
+        public string Name {set; get}
+        public string Phone {set; get}
+        public string Email {set; get}
+        public int Role {set; get}
+
+        public int ROLE_SALE = 1;
+        public int ROLE_ACCOUNTANT = 2;
+
+        
+
     }
 }
