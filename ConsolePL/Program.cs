2a16dd023108f4f04cd44bbdfaae5e3199be3707
tiendx161202 <dangxuantien161202@gmail.com>
2021-09-28 21:56:11 +0700

    intomoney

--- ConsolePL/Program.cs
+++ ConsolePL/Program.cs
@@ -33,24 +33,20 @@ namespace ConsolePL
             Console.WriteLine("Customer Address:      {0}", invoice.InvoiceCustomer.CustomerAddress);
             Console.WriteLine("======================================================");
 
-<<<<<<< HEAD
+
             double total = 0;
             int no = 0;
-=======
-            decimal? total = 0;
->>>>>>> fd40e838b7529a0b5e89bfa557ef53fba5f156c2
+
             var table = new ConsoleTable("NO", "LAPTOP", "QUANITY", "PRICE");
             IFormatProvider info = System.Globalization.CultureInfo.GetCultureInfo("vi-VN");
             foreach (var l in invoice.LaptopList)
             {
-                int no = 1;
+                ++no;
                 table.AddRow(no.ToString(), l.Name, l.Quanity, string.Format(info, "{0:c}", l.Price));
-<<<<<<< HEAD
+
                 total +=(double) l.Price;
-=======
-                total += l.Price;
-                no++;
->>>>>>> fd40e838b7529a0b5e89bfa557ef53fba5f156c2
+
+ 
             }
             table.Write(ConsoleTables.Format.Alternative);
             Console.WriteLine(string.Format(info, "Total invoice:  {0:c}", total));
@@ -87,20 +83,7 @@ namespace ConsolePL
             }
             DisplayInvoice(invoice);
 
-<<<<<<< HEAD
 
-            Console.Write("Reconfirm the invoice !!! (Y/N): ");
-            string choices =Console.ReadLine();
-            switch (choices)
-            {
-                case "Y":
-                break;
-                case "N":
-                break;
-                default:
-                break;
-            }
-=======
             while (true)
             {
                 Console.Write("Are you sure to confirm this invoice? (Y/N): ");
@@ -120,7 +103,7 @@ namespace ConsolePL
                 break;
             } 
             Console.WriteLine("HHHHHHHHHHHHHHHHH");
->>>>>>> fd40e838b7529a0b5e89bfa557ef53fba5f156c2
+
 
             Console.ReadKey();
 
